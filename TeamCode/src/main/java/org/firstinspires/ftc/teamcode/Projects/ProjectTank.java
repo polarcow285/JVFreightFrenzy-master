b0adc1f4968c10b9832aa8452be140624824791c
Jacob <jacoblax04@gmail.com>
2021-10-11 11:57:39 -0700

    Wrote project files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Projects/ProjectTank.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Projects/ProjectTank.java
@@ -6,20 +6,33 @@ public class ProjectTank extends Project{
     //Project file for JV Robot
 
     //Setup motors
-
+    public DcMotor rightMotor = null;
+    public DcMotor leftMotor = null;
     @Override
     public void init(HardwareMap ahwMap) {
         //Save reference to Hardware map
         hwMap = ahwMap;
 
         //Define and Initialize Motors
+        rightMotor = hwMap.dcMotor.get("rightMotor");
+        leftMotor = hwMap.dcMotor.get("leftMotor");
 
         //Setup Motor directions and Encoder settings
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightMotor.setDirection(DcMotor.Direction.FORWARD);
 
+        leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         // Set all motors to zero power
+
+        Stop();
     }
 
     public void Stop(){
-
+        rightMotor.setPower(0);
+        leftMotor.setPower(0);
     }
 }
