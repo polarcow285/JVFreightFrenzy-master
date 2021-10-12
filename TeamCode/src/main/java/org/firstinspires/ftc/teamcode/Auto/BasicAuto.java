02f752d6db22ecfb15b44800e2cca55b75b2f889
Strahdvonzarovich756 <78618866+Strahdvonzarovich756@users.noreply.github.com>
2021-10-12 15:39:35 -0700

    Coded simple autonomous commands and method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/BasicAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/BasicAuto.java
@@ -17,7 +17,19 @@ public class BasicAuto extends LinearOpMode{
         waitForStart();
 
         //write autonomous code here
+        commandBothMotors(1, 3);
+        commandBothMotors(0, 2);
+        commandBothMotors(-0.5, 8);
+
+        robot.rightMotor.setPower(0);
+        robot.leftMotor.setPower(0);
 
 
+
+    }
+    void commandBothMotors(int pwr, int time){
+        robot.leftMotor.setPower(pwr);
+        robot.rightMotor.setPower(pwr);
+        sleep(milliseconds: time*1000);
     }
 }
