e162c0fe4c9c3135113bed46cd7e5390dcbd58d0
Strahdvonzarovich756 <78618866+Strahdvonzarovich756@users.noreply.github.com>
2021-11-16 15:54:34 -0800

    Coded simple autonomous commands and method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TestVuforiaAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TestVuforiaAuto.java
@@ -2,6 +2,14 @@ package org.firstinspires.ftc.teamcode.Auto;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.Projects.ProjectPushbotTest;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+
 
 @Autonomous(name = "TestVuforiaAuto")
 public class TestVuforiaAuto extends LinearOpMode{
@@ -9,7 +17,15 @@ public class TestVuforiaAuto extends LinearOpMode{
 
     @Override
     public void runOpMode() throws InterruptedException {
-        robot.init(hardwareMap);
+        robot.camera = hardwareMap.get(WebcamName.class, "webcam");
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+        parameters.vuforiaLicenseKey = "AXWZxrb/////AAABmV1b6nGOGE8vkm2XisMdt8A7XFbo/8CaNRpnqMV1OjTd9o4q2mJEZx+9qgNSNcyzX/Buk/gfG4syevzdWqJ79hApY/tpJS/It5f64LRXlMjjcsdqFSdc9Im/jfiNbb7P6f5vXXCzvvabLyfi9ZMhDUp71mXnzS0CEnNjpe8ttMAW9kysjNCLYNucNimxeAjbt7wJhg8bkvUZevmnKH4xYbY6TyAeMIX5HEe0YGfCVh88J0XLUoKMT6jWT4I8O2ltVL3DH0VJlgnFAS3+LMfYIbDHE+rsH7eo6ZsKQLPbsRr+wjSQHyAUJzA/5KerUeHQdnOpsDz4t34KpMvP4EbIu32MRi7XwGBpZoiMJkSM8fvu";
+        parameters.cameraName = robot.camera;
+
+        VuforiaLocalizer vuforia = ClassFactory.getInstance().createVuforia(parameters);
+        VuforiaTrackables trackables = vuforia.loadTrackablesFromAsset(" /*name of the dat/xml file found in FTCRobotController > assets *\ ");
 
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Projects/ProjectPushbotTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Projects/ProjectPushbotTest.java
@@ -13,15 +13,25 @@ public class ProjectPushbotTest extends Project{
 
 
         //Define and Initialize Motors
-
+        rightMotor = hwMap.dcMotor.get("rightMotor");
+        leftMotor = hwMap.dcMotor.get("leftMotor");
         //Setup Motor directions and Encoder settings
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        rightMotor.setDirection(DcMotor.Direction.FORWARD);
+
+        leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        leftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         // Set all motors to zero power
         Stop();
     }
 
     public void Stop(){
-
+        rightMotor.setPower(0);
+        leftMotor.setPower(0);
 
     }
 }
