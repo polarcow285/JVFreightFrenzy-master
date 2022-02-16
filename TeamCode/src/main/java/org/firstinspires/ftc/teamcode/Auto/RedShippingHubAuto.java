650f90d95675b8913c4ccff7588e888b33a9047b
Loie White <loiewhite84@gmail.com>
2022-02-16 13:05:44 -0800

    new auto file(not written)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/RedShippingHubAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/RedShippingHubAuto.java
@@ -1,47 +1,51 @@
-package org.firstinspires.ftc.teamcode.Teleop;
+package org.firstinspires.ftc.teamcode.Auto;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-
 import org.firstinspires.ftc.teamcode.Projects.ProjectTank;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp(name="RedShippingHubAuto")
-public class RedShippingHubAuto extends LinearOpMode{
+@Autonomous(name="RedShippingHubAuto")
+//"tag" that is displayed on driver hub
+public class RedShippingHubAuto extends LinearOpMode {
+    //creating robot object
     public ProjectTank robot = new ProjectTank();
 
     @Override
     public void runOpMode() throws InterruptedException {
-        //initialize hardware map
-        robot.init(hardwareMap);
-        
         /*
-        turn 90 
+        turn 90
         move forward
         turn left 90
         move forward to shipping hub
         lift arm
         release block
         arm down
-        
+
         park??
         */
-        
     }
+
     public void encoderDrive(double speed,
-                             double armCounts) {
-        int newTarget;
+                             double leftCounts, double rightCounts) {
+        int newLeftTarget;
+        int newRightTarget;
 
         // Ensure that the opmode is still active
         if (opModeIsActive()) {
 
             // Determine new target position, and pass to motor controller
-            newTarget = robot.armMotor.getCurrentPosition() + (int)(armCounts);
-            robot.armMotor.setTargetPosition(newTarget);
+            newLeftTarget = robot.leftMotor.getCurrentPosition() + (int) (leftCounts);
+            newRightTarget = robot.rightMotor.getCurrentPosition() + (int) (rightCounts);
+            robot.leftMotor.setTargetPosition(newLeftTarget);
+            robot.rightMotor.setTargetPosition(newRightTarget);
 
             // Turn On RUN_TO_POSITION
-            robot.armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-            robot.armMotor.setPower(Math.abs(speed));
+            robot.leftMotor.setPower(Math.abs(speed));
+            robot.rightMotor.setPower(Math.abs(speed));
 
             // keep looping while we are still active, and there is time left, and both motors are running.
             // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
@@ -49,22 +53,24 @@ public class RedShippingHubAuto extends LinearOpMode{
             // always end the motion as soon as possible.
             // However, if you require that BOTH motors have finished their moves before the robot continues
             // onto the next step, use (isBusy() || isBusy()) in the loop test.
-            /*while (opModeIsActive() &&
-                    (robot.armMotor.isBusy())) {
+            while (opModeIsActive() &&
+                    (robot.leftMotor.isBusy() && robot.rightMotor.isBusy())) {
 
                 // Display it for the driver.
-                telemetry.addData("Path2", robot.armMotor.getCurrentPosition());
+                telemetry.addData("Path1", "Running to %7d :%7d", newLeftTarget, newRightTarget);
+                telemetry.addData("Path2", "Running at %7d :%7d",
+                        robot.leftMotor.getCurrentPosition(),
+                        robot.rightMotor.getCurrentPosition());
                 telemetry.update();
             }
 
             // Stop all motion;
-            robot.armMotor.setPower(0);
-               */
-            // Turn off RUN_TO_POSITION
-            robot.armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.leftMotor.setPower(0);
+            robot.rightMotor.setPower(0);
 
-            //  sleep(250);   // optional pause after each move
+            // Turn off RUN_TO_POSITION
+            robot.leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         }
     }
-    //Stop robot motors when game is finished
 }
